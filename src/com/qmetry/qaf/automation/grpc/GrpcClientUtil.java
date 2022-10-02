d30bcb9c41b709cfc817f110dbdbd137d327b7fe
Chirag Jayswal <cjayswal@gmail.com>
2022-10-02 12:35:44 -0700

    Added steps and utility methods

--- src/com/qmetry/qaf/automation/grpc/GrpcClientUtil.java
+++ src/com/qmetry/qaf/automation/grpc/GrpcClientUtil.java
@@ -8,19 +8,38 @@ import static com.qmetry.qaf.automation.core.ConfigurationManager.getBundle;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.json.JSONException;
+
+import com.google.common.base.Joiner;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.protobuf.DescriptorProtos.FileDescriptorSet;
+import com.google.protobuf.Descriptors.Descriptor;
+import com.google.protobuf.Descriptors.FieldDescriptor;
+import com.google.protobuf.Descriptors.MethodDescriptor;
+import com.google.protobuf.Descriptors.ServiceDescriptor;
 import com.qmetry.qaf.automation.core.AutomationError;
+import com.qmetry.qaf.automation.core.ConfigurationManager;
 import com.qmetry.qaf.automation.core.LoggingBean;
 import com.qmetry.qaf.automation.core.TestBaseProvider;
+import com.qmetry.qaf.automation.util.FileUtil;
+import com.qmetry.qaf.automation.util.JSONUtil;
 import com.qmetry.qaf.automation.util.StringUtil;
 
 import io.grpc.StatusException;
@@ -31,6 +50,7 @@ import me.dinowernli.grpc.polyglot.config.ConfigurationLoader;
 import me.dinowernli.grpc.polyglot.io.Output;
 import me.dinowernli.grpc.polyglot.protobuf.ProtocInvoker;
 import me.dinowernli.grpc.polyglot.protobuf.ProtocInvoker.ProtocInvocationException;
+import me.dinowernli.grpc.polyglot.protobuf.ServiceResolver;
 import polyglot.ConfigProto.Configuration;
 import polyglot.ConfigProto.Configuration.Builder;
 import polyglot.ConfigProto.ProtoConfiguration;
@@ -48,9 +68,18 @@ public class GrpcClientUtil {
 		String message = "";
 		try {
 
-//			if (StringUtil.isBlank(request.getBody())) {
-//				request.setBody("{}");
-//			}
+			if (StringUtil.isBlank(request.getBody())) {
+				request.setBody("{}");
+			}
+
+			if (StringUtil.isBlank(request.getProto_discovery_root())) {
+				String protoFilePath = getProtoFilePath(request.getEndPoint());
+				if(StringUtil.isNotBlank(protoFilePath)) {
+					request.setProto_discovery_root(protoFilePath);
+				}else {
+					request.setProto_discovery_root(getBundle().getString("grpc.proto_discovery_root"));
+				}
+			}
 			final byte[] payload = request.getBody().getBytes();
 			final ByteArrayInputStream payloadStream = new ByteArrayInputStream(payload);
 			System.setIn(payloadStream);
@@ -71,7 +100,6 @@ public class GrpcClientUtil {
 			configBuilder.getProtoConfigBuilder()
 					.setProtoDiscoveryRoot(proto_discovery_root);
 			Configuration config = configBuilder.build();
-			// Output output = Output.forConfiguration(config.getOutputConfig());
 
 			ProtoConfiguration protoConfig = config.getProtoConfig();
 			try (PrintStream ps = new PrintStream(baos, true, utf8)) {
@@ -81,7 +109,7 @@ public class GrpcClientUtil {
 
 				try {
 					ServiceCall.callEndpoint(output, protoConfig, Optional.of(request.getBaseUrl()),
-							Optional.of(request.getQualifiedMethod()),
+							Optional.of(request.getEndPoint()),
 							maybeInputPath(proto_discovery_root),
 							maybeInputPath(config_set_path), ImmutableList.of(),
 							config.getCallConfig());
@@ -122,17 +150,69 @@ public class GrpcClientUtil {
 		return response;
 	}
 
-	public static GrpcResponse getResponse() {
+	public static GrpcResponse getGrpcResponse() {
 		return (GrpcResponse) TestBaseProvider.instance().get().getContext().getProperty(GRPC_RESPONSE_KEY);
 	}
 	
+	public static List<MethodDescriptor> listMethods(String protoDiscoveryRoot) throws ProtocInvocationException {
+
+		final ConfigurationLoader configLoader = maybeInputPath(
+				getBundle().getString("grpc.config_set_path", "resources/proto/grpc.config.json"))
+				.map(ConfigurationLoader::forFile).orElseGet(() -> ConfigurationLoader.forDefaultConfigSet());// .withOverrides(arguments
+																												// )
+
+		Builder configBuilder = Optional.ofNullable(getBundle().getString("grpc.config_name"))
+				.map(configLoader::getNamedConfiguration).orElseGet(() -> configLoader.getDefaultConfiguration())
+				.toBuilder();
+		configBuilder.getProtoConfigBuilder().setProtoDiscoveryRoot(protoDiscoveryRoot);
+
+		Configuration config = configBuilder.build();
+
+		FileDescriptorSet fileDescriptorSet = getFileDescriptorSet(config.getProtoConfig());
+
+		ServiceResolver serviceResolver = ServiceResolver.fromFileDescriptorSet(fileDescriptorSet);
+		List<MethodDescriptor> methods = new ArrayList<>();
+		for (ServiceDescriptor descriptor : serviceResolver.listServices()) {
+
+			try {
+				methods.addAll(descriptor.getMethods());
+			} catch (Throwable e) {
+				e.printStackTrace();
+				System.err.println("ERROR:: " + descriptor.getFullName());
+			}
+		}
+		return methods;
+	}
+	public static String renderDescriptor(Descriptor descriptor, String indent) {
+		if (descriptor.getFields().size() == 0) {
+			return indent + "<empty>";
+		}
+
+		List<String> fieldsAsStrings = descriptor.getFields().stream()
+				.map(field -> renderDescriptor(field, indent + "  ")).collect(Collectors.toList());
+
+		return Joiner.on(System.lineSeparator()).join(fieldsAsStrings);
+	}
+
+	private static String renderDescriptor(FieldDescriptor descriptor, String indent) {
+		String isOpt = descriptor.isOptional() ? "<optional>" : "<required>";
+		String isRep = descriptor.isRepeated() ? "<repeated>" : "<single>";
+		String fieldPrefix = indent + descriptor.getJsonName() + "[" + isOpt + " " + isRep + "]";
+
+		if (descriptor.getJavaType() == FieldDescriptor.JavaType.MESSAGE) {
+			return fieldPrefix + " {" + System.lineSeparator()
+					+ renderDescriptor(descriptor.getMessageType(), indent + "  ") + System.lineSeparator() + indent
+					+ "}";
+
+		} else if (descriptor.getJavaType() == FieldDescriptor.JavaType.ENUM) {
+			return fieldPrefix + ": " + descriptor.getEnumType().getValues();
+
+		} else {
+			return fieldPrefix + ": " + descriptor.getJavaType();
+		}
+	}
 	public static void list() {
-		
 		try {
-			long stTime=System.currentTimeMillis();
-			
-
-
 			final ConfigurationLoader configLoader = maybeInputPath(getBundle().getString("grpc.config_set_path"))
 					.map(ConfigurationLoader::forFile).orElseGet(() -> ConfigurationLoader.forDefaultConfigSet());// .withOverrides(arguments);
 
@@ -142,23 +222,15 @@ public class GrpcClientUtil {
 			configBuilder.getProtoConfigBuilder()
 					.setProtoDiscoveryRoot(getBundle().getString("grpc.proto_discovery_root"));
 			Configuration config = configBuilder.build();
-			// Output output = Output.forConfiguration(config.getOutputConfig());
-
 			ProtoConfiguration protoConfig = config.getProtoConfig();
 				Output output = Output.forStream(new PrintStream(System.out) {public void print(String s) {super.print("Start::" +s);};});
 
 				try {
-			          FileDescriptorSet fileDescriptorSet = getFileDescriptorSet(config.getProtoConfig());
-
+			        FileDescriptorSet fileDescriptorSet = getFileDescriptorSet(config.getProtoConfig());
 					ServiceList.listServices(output, fileDescriptorSet, protoConfig.getProtoDiscoveryRoot(), Optional.ofNullable(null), Optional.ofNullable(null), Optional.ofNullable(true));
-//			          ServiceList.listServices(
-//			              commandLineOutput,
-//			              fileDescriptorSet, config.getProtoConfig().getProtoDiscoveryRoot(),
-//			              arguments.serviceFilter(), arguments.methodFilter(), arguments.withMessage());
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-			System.err.println("Time Taken: " + (System.currentTimeMillis() - stTime));
 
 		} catch (Exception | Error e) {
 			throw new AutomationError(e);
@@ -168,10 +240,6 @@ public class GrpcClientUtil {
 	public static void list(String file) {
 		
 		try {
-			long stTime=System.currentTimeMillis();
-			
-
-
 			final ConfigurationLoader configLoader = maybeInputPath(getBundle().getString("grpc.config_set_path"))
 					.map(ConfigurationLoader::forFile).orElseGet(() -> ConfigurationLoader.forDefaultConfigSet());// .withOverrides(arguments);
 
@@ -181,44 +249,32 @@ public class GrpcClientUtil {
 			configBuilder.getProtoConfigBuilder()
 					.setProtoDiscoveryRoot(getBundle().getString("grpc.proto_discovery_root",new File(file).getParentFile().getPath()));
 			Configuration config = configBuilder.build();
-			// Output output = Output.forConfiguration(config.getOutputConfig());
 
 			ProtoConfiguration protoConfig = config.getProtoConfig();
 				Output output = Output.forStream(new PrintStream(System.out) {public void print(String s) {super.print("Start::" +s);};});
 
 				try {
-			          FileDescriptorSet fileDescriptorSet = getFileDescriptorSet(config.getProtoConfig());
-
+			        FileDescriptorSet fileDescriptorSet = getFileDescriptorSet(config.getProtoConfig());
 					ServiceList.listServices(output, fileDescriptorSet, protoConfig.getProtoDiscoveryRoot(), Optional.ofNullable(null), Optional.ofNullable(null), Optional.ofNullable(true));
-//			          ServiceList.listServices(
-//			              commandLineOutput,
-//			              fileDescriptorSet, config.getProtoConfig().getProtoDiscoveryRoot(),
-//			              arguments.serviceFilter(), arguments.methodFilter(), arguments.withMessage());
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-			System.err.println("Time Taken: " + (System.currentTimeMillis() - stTime));
-
 		} catch (Exception | Error e) {
 			throw new AutomationError(e);
 		}
 	}
 
-	 /** Invokes protoc and returns a {@link FileDescriptorSet} used for discovery. */
-	  private static FileDescriptorSet getFileDescriptorSet(ProtoConfiguration protoConfig) {
-	    try {
-	    	ProtocInvoker invoker = ProtocInvoker.forConfig(protoConfig);
-	      return invoker.invoke();
-	    } catch (ProtocInvocationException e) {
-	      throw new RuntimeException("Failed to invoke the protoc binary", e);
-	    }
+	 /** Invokes protoc and returns a {@link FileDescriptorSet} used for discovery. 
+	 * @throws ProtocInvocationException */
+	  private static FileDescriptorSet getFileDescriptorSet(ProtoConfiguration protoConfig) throws ProtocInvocationException {
+	    ProtocInvoker invoker = ProtocInvoker.forConfig(protoConfig);
+	    return invoker.invoke();
 	  }
 
 	private static Optional<Path> maybeOutputPath(String rawPath) {
 		if (rawPath == null) {
 			return Optional.empty();
 		}
-		// Path path = Paths.get(rawPath);
 		return Optional.of(Paths.get(rawPath));
 	}
 
@@ -239,6 +295,59 @@ public class GrpcClientUtil {
 		}
 		// Couldn't find a cause with a Status
 		return t;
+	}
+	
+	private static String getProtoFilePath(String endpoint) {
+		String methodDescriptorKey="grpcmethod."+endpoint.replace("/", ".").trim();
+
+		String methodDescriptor=getBundle().getString(methodDescriptorKey);
+		if(StringUtil.isBlank(methodDescriptor)) {
+    		String root = ConfigurationManager.getBundle().getString("grpc.proto_discovery_root", "resources/proto");
+    		File discoveryFile = new File(root,"proto-discovery.properties");
+
+    		if(discoveryFile.exists()) {
+    			ConfigurationManager.getBundle().load(new String[] {discoveryFile.getPath()});
+				methodDescriptor=getBundle().getString(methodDescriptorKey);
+    		}
+    		if(StringUtil.isBlank(methodDescriptor)) {
+    			Collection<File> allProtoFiles = FileUtil.getFiles(new File(root), "proto", true); 
+    			PropertiesConfiguration prop = new PropertiesConfiguration();
+    			prop.setEncoding("UTF-8");
+
+    			//Properties prop = new Properties();
+    			try(FileInputStream fis = new FileInputStream(discoveryFile)) {
+					prop.load(fis);
+				} catch (IOException | ConfigurationException e1) {
+				}
+    			for(File protoFile : allProtoFiles) {
+					try {
+						List<MethodDescriptor> methods = listMethods(protoFile.getPath());
+						for(MethodDescriptor m:methods) {
+							int lastDot = m.getFullName().lastIndexOf('.');
+							StringBuilder methodName = new StringBuilder(m.getFullName());
+							methodName.replace(lastDot, lastDot+1, "/");
+							prop.setProperty("grpcmethod." +m.getFullName(), String.format("{'endpoint':'%s','proto_discovery_root':'%s'}",methodName, protoFile.getPath()));
+						}
+					} catch (ProtocInvocationException e) {
+					}
+    			}
+    			try(FileOutputStream out = new FileOutputStream(discoveryFile)) {
+					prop.save(out, "UTF-8");
+				} catch (IOException | ConfigurationException e) {
+					e.printStackTrace();
+				}
+    			ConfigurationManager.getBundle().load(new String[] {discoveryFile.getPath()});
+				methodDescriptor=getBundle().getString(methodDescriptorKey);
+    		}
+		}
+		if(StringUtil.isNotBlank(methodDescriptor)) {
+			try {
+				return (String) JSONUtil.toMap(methodDescriptor).getOrDefault("proto_discovery_root","");
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		return "";
 	}
 
 }
--- src/com/qmetry/qaf/automation/step/GrpcSteps.java
+++ src/com/qmetry/qaf/automation/step/GrpcSteps.java
@@ -21,23 +21,33 @@
  ******************************************************************************/
 package com.qmetry.qaf.automation.step;
 
+import static com.qmetry.qaf.automation.core.ConfigurationManager.getBundle;
 import static com.qmetry.qaf.automation.grpc.GrpcClientUtil.call;
-import static com.qmetry.qaf.automation.grpc.GrpcClientUtil.getResponse;
+import static com.qmetry.qaf.automation.grpc.GrpcClientUtil.getGrpcResponse;
+import static com.qmetry.qaf.automation.grpc.GrpcClientUtil.listMethods;
+import static com.qmetry.qaf.automation.grpc.GrpcClientUtil.renderDescriptor;
 import static com.qmetry.qaf.automation.util.Validator.assertThat;
 
 import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.hamcrest.Matchers;
 import org.json.JSONObject;
 
 import com.google.common.collect.ImmutableMap;
 import com.jayway.jsonpath.JsonPath;
+import com.qmetry.qaf.automation.core.ConfigurationManager;
+import com.qmetry.qaf.automation.grpc.GrpcClientUtil;
 import com.qmetry.qaf.automation.grpc.GrpcRequestBean;
 import com.qmetry.qaf.automation.grpc.GrpcResponse;
+import com.qmetry.qaf.automation.util.StringUtil;
 import com.qmetry.qaf.automation.util.Validator;
 
-
+import me.dinowernli.grpc.polyglot.protobuf.ProtocInvoker.ProtocInvocationException;
 /**
  * 
  * @author chirag.jayswal
@@ -45,10 +55,66 @@ import com.qmetry.qaf.automation.util.Validator;
  */
 public class GrpcSteps {
 
+	@QAFTestStep(description = "client sends a {grpc-endpoint} request to the server")
+	public static GrpcResponse callGrpcMethod(String endpoint) {
+		return callGrpcMethodUsingData(endpoint, "{}", ImmutableMap.of());
+	}
+
+	@QAFTestStep(description = "client sends a {grpc-endpoint} request  with {payload} to the server")
+	public static GrpcResponse callGrpcMethodWithPayload(String endpoint, String payload) {
+		return callGrpcMethodUsingData(endpoint, payload, ImmutableMap.of());
+	}
+
+    @QAFTestStep(description = "client sends a {grpc-endpoint} request  with {payload} to the server using {data}")
+	public static GrpcResponse callGrpcMethodUsingData(String endpoint, String payload, Map<String, Object> data){
+    	String server = getBundle().getString("grpc.baseurl",getBundle().getString("grpc.server"));
+    	if(StringUtil.countMatches(endpoint, "/")>1) {
+    		String[] parts = endpoint.split("/",2);
+    		if(StringUtil.isNotBlank(parts[0])) {
+    			server=parts[0];
+    		}
+    		endpoint=parts[1];
+    	}
+		GrpcRequestBean bean = GrpcRequestBean.from(String.format("{'endpoint':'%s'}", endpoint)).with(data);
+		bean.setBaseUrl(server);
+		if(StringUtil.isNotBlank(payload)) {
+			bean.setBody(payload);
+		}
+		return GrpcClientUtil.call(bean);
+	}
+
+	@QAFTestStep(description = "get grpc method names from {proto}")
+	public static List<String> getGRPCMethodNames(String protofile) {
+		try {
+			ConfigurationManager.getBundle().setProperty("grpc.proto_discovery_root", "resources/proto");
+
+			return listMethods(protofile).stream().map(m -> m.getName()).collect(Collectors.toList());
+		} catch (ProtocInvocationException e) {
+			e.printStackTrace();
+			return Collections.emptyList();
+		}
+	}
+
+	@QAFTestStep(description = "get grpc method {method} details from {proto}")
+	public static Map<String, String> getGRPCMethodDetails(String protofile, String method) {
+		try {
+			return listMethods(protofile).stream().filter(m -> m.getName().equalsIgnoreCase(method)).map(m -> {
+				Map<String, String> details = new HashMap<>();
+				details.put("baseurl", ConfigurationManager.getBundle().getString("grpc.server", "grpc.server"));
+				details.put("endpoint", m.getFullName().split("." + method)[0] + "/" + method);
+				details.put("input", renderDescriptor(m.getInputType(), ""));
+				return details;
+			}).findFirst().get();
+
+		} catch (ProtocInvocationException e) {
+			e.printStackTrace();
+			return Collections.emptyMap();
+		}
+	}
+
 	@QAFTestStep(description = "client does {reqcall} grpc call")
 	public static void doGrpcClientCall(Object request) {
 		doGrpcClientCall(request, ImmutableMap.of());
-
 	}
 
 	@QAFTestStep(description = "client does {reqcall} grpc call with {data}", stepName = "doGrpcClientCallWithData")
@@ -57,9 +123,6 @@ public class GrpcSteps {
 		return call(bean);
 	}
 
-	public static void listMethods(String protoFile) {
-		
-	}
 	/**
 	 * This method check for the response status of GRPC call
 	 * <p>
@@ -78,8 +141,8 @@ public class GrpcSteps {
 	 * @see {@link Code StatusCodes}
 	 */
 	@QAFTestStep(description = "grpc response status is {status}")
-	public static void responseStatusIs(String status) {
-		assertThat("Response Status", getResponse().getStatus().getCode().name(), Matchers.equalToIgnoringCase(status));
+	public static void grpcResponseStatusIs(String status) {
+		assertThat("Response Status", getGrpcResponse().getStatus().getCode().name(), Matchers.equalToIgnoringCase(status));
 	}
 
 	/**
@@ -99,8 +162,8 @@ public class GrpcSteps {
 	 * @param description : {0} : description string
 	 */
 	@QAFTestStep(description = "grpc response description is {description}")
-	public static void responseDescriptionIs(String description) {
-		assertThat("Response Status", getResponse().getStatus().getDescription(),
+	public static void grpcResponseDescriptionIs(String description) {
+		assertThat("Response Status", getGrpcResponse().getStatus().getDescription(),
 				Matchers.equalToIgnoringCase(description));
 	}
 
@@ -120,7 +183,7 @@ public class GrpcSteps {
 	public static void grpcResponseHasJsonPath(String path) {
 		if (!path.startsWith("$"))
 			path = "$." + path;
-		assertThat("Response Body has " + path, hasJsonPath(getResponse().getMessageBody(), path),
+		assertThat("Response Body has " + path, hasJsonPath(getGrpcResponse().getMessageBody(), path),
 				Matchers.equalTo(true));
 	}
 
@@ -140,7 +203,7 @@ public class GrpcSteps {
 	public static void grpcResponseNotHaveJsonPath(String path) {
 		if (!path.startsWith("$"))
 			path = "$." + path;
-		assertThat("Response Body has not " + path, hasJsonPath(getResponse().getMessageBody(), path),
+		assertThat("Response Body has not " + path, hasJsonPath(getGrpcResponse().getMessageBody(), path),
 				Matchers.equalTo(false));
 	}
 
@@ -161,7 +224,7 @@ public class GrpcSteps {
 	public static void grpcResponseHasKeyWithValue(Object expectedValue, String path) {
 		if (!path.startsWith("$"))
 			path = "$." + path;
-		Object actual = JsonPath.read(getResponse().getMessageBody(), path);
+		Object actual = JsonPath.read(getGrpcResponse().getMessageBody(), path);
 		if (null != actual && Number.class.isAssignableFrom(actual.getClass())) {
 			assertThat(new BigDecimal(String.valueOf(actual)),
 					Matchers.equalTo(new BigDecimal(String.valueOf(expectedValue))));
@@ -171,24 +234,32 @@ public class GrpcSteps {
 	}
 
 	@QAFTestStep(description = "grpc response contains json {expectedJsonStr}")
-	public static void assertResponseContains(String expectedJsonStr) {
-		Validator.assertJsonContains(getResponse().getMessageBody(), expectedJsonStr);
+	public static void assertGrpcResponseContains(String expectedJsonStr) {
+		Validator.assertJsonContains(getGrpcResponse().getMessageBody(), expectedJsonStr);
 	}
 
 	@QAFTestStep(description = "grpc response should contain json {expectedJsonStr}")
-	public static void verifyResponseContains(String expectedJsonStr) {
-		Validator.verifyJsonContains(getResponse().getMessageBody(), expectedJsonStr);
+	public static void verifyGrpcResponseContains(String expectedJsonStr) {
+		Validator.verifyJsonContains(getGrpcResponse().getMessageBody(), expectedJsonStr);
 	}
 
 	@QAFTestStep(description = "grpc response matches json {expectedJsonStr}")
-	public static void assertResponseMatches(String expectedJsonStr) {
-		Validator.assertJsonMatches(getResponse().getMessageBody(), expectedJsonStr);
+	public static void assertGrpcResponseMatches(String expectedJsonStr) {
+		Validator.assertJsonMatches(getGrpcResponse().getMessageBody(), expectedJsonStr);
 	}
 
 	@QAFTestStep(description = "grpc response should match json {expectedJsonStr}")
-	public static void verifyResponseMatches(String expectedJsonStr) {
-		Validator.verifyJsonMatches(getResponse().getMessageBody(), expectedJsonStr);
+	public static void verifyGrpcResponseMatches(String expectedJsonStr) {
+		Validator.verifyJsonMatches(getGrpcResponse().getMessageBody(), expectedJsonStr);
 	}
+
+    @QAFTestStep(description = "resolve grpc request call {0} with {data}")
+    public GrpcRequestBean resolveGrpcCallwithData(Object request, Map<String, Object> data) {
+    	GrpcRequestBean bean = GrpcRequestBean.from(request);
+		bean.fillData(request);
+		bean.resolveParameters(data);
+		return bean;
+    }
 	/**
 	 * @param json
 	 * @param path
